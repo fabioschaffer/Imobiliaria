0dc6e506b823a8661437d1a0b62a0e1854e83cfe
Fabio Schaffer <fabioschaffer@gmail.com>
2025-11-16 17:06:16 -0300

    criação migrations.

--- BackEnd/Repository/Contexto/AplicacaoDbContext.cs
+++ BackEnd/Repository/Contexto/AplicacaoDbContext.cs
@@ -4,6 +4,10 @@ using Microsoft.EntityFrameworkCore;
 namespace Repositorio.Contexto;
 public class AplicacaoDbContext : DbContext {
 
+    public AplicacaoDbContext(DbContextOptions<AplicacaoDbContext> options)
+      : base(options) {
+    }
+
     public DbSet<UnidadeFederacao> UnidadesFederacao { get; set; }
 
 }
---
+++ BackEnd/Repository/Migrations/20251116195705_migracao_01.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracao_01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnidadesFederacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadesFederacao", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnidadesFederacao");
+        }
+    }
+}
