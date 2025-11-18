adad566c96eea3b18d6a8ba2de93d773380aeb46
Fabio Schaffer <fabioschaffer@gmail.com>
2025-11-17 22:10:13 -0300

    Atualização BD.

--- BackEnd/Domain/UnidadeFederacao.cs
+++ BackEnd/Domain/UnidadeFederacao.cs
@@ -3,8 +3,6 @@
         public int Id { get; private set; }
         public string Nome { get; private set; }
 
-        public int teste { get; set; }
-
         public UnidadeFederacao(string nome) {
             Nome = nome;
         }
--- BackEnd/Repository/Migrations_Commands/Migrations_Commands.cs
+++ BackEnd/Repository/Migrations_Commands/Migrations_Commands.cs
@@ -1,8 +1,10 @@
 ﻿
 /*
 
-dotnet ef migrations add migration_17_11_25_01 --context AplicacaoDbContext --startup-project .\Api\API.csproj --project .\Repository\Repositorio.csproj
+dotnet ef migrations add migration_17_11_25_02 --context AplicacaoDbContext --startup-project .\Api\API.csproj --project .\Repository\Repositorio.csproj
 
 dotnet ef database update migration_17_11_25_01 --context AplicacaoDbContext --startup-project .\Api\API.csproj --project .\Repository\Repositorio.csproj
+
+dotnet ef database update --context AplicacaoDbContext --startup-project .\Api\API.csproj --project .\Repository\Repositorio.csproj
 
 */
