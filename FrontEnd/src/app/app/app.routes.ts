f9b7f6f5e201784bc6f5e59b685b8bb9d8ae63f9
Fabio Schaffer <fabioschaffer@gmail.com>
2025-11-19 23:27:00 -0300

    Criação componente unidade-federação. instalação do bootstrap.

---
+++ FrontEnd/src/app/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Unidadefederacao } from '../unidadefederacao/unidadefederacao';
+
+export const routes: Routes = [
+    { path: 'unidadefederacao', component: Unidadefederacao  }
+];
---
+++ FrontEnd/src/app/unidadefederacao/unidadefederacao.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unidadefederacao',
+  imports: [],
+  templateUrl: './unidadefederacao.html',
+  styleUrl: './unidadefederacao.scss',
+})
+export class Unidadefederacao {
+
+}
