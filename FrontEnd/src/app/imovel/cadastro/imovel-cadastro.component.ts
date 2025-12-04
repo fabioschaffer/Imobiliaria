af128ca4cd484cd4f5d6b4feb2ed0f36342a347f
Fabio Schaffer <fabioschaffer@gmail.com>
2025-12-04 17:22:04 -0300

    Implementando características do imóvel.

--- FrontEnd/src/app/imovel/cadastro/imovel-cadastro.component.ts
+++ FrontEnd/src/app/imovel/cadastro/imovel-cadastro.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { somenteNumerosDirective } from '../../../Diretivas/somente-numeros.diretiva';
 import { DuasDecimaisDirective } from '../../../Diretivas/duas-decimais.diretiva';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'imovel-cadastro.component',
   standalone: true,
-  imports: [ReactiveFormsModule, somenteNumerosDirective, DuasDecimaisDirective],
+  imports: [ReactiveFormsModule, somenteNumerosDirective, DuasDecimaisDirective, CommonModule],
   templateUrl: './imovel-cadastro.component.html',
   styleUrl: './imovel-cadastro.component.scss',
 })
 export class ImovelCadastroComponent {
   dadosIniciaisForm: FormGroup;
+
+  caracteristicas: { descricao: string; valor: number }[] = [
+    { descricao: 'Item 1', valor: 100 },
+    { descricao: 'Item 2', valor: 200 },
+    { descricao: 'Item 3', valor: 300 }
+  ];
 
   constructor() {
     this.dadosIniciaisForm = new FormGroup({
