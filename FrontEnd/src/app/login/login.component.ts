251c28b62fa041aed6b62af8ebb75cedbfe2abba
Fabio Schaffer <fabioschaffer@gmail.com>
2025-12-25 20:50:18 -0300

    Ajustes componente de login.

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  standalone: true,
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+
+  camposForm: FormGroup;
+
+
+  constructor(private router: Router) {
+    this.camposForm = new FormGroup({
+      login: new FormControl('', Validators.required),
+      senha: new FormControl('', Validators.required),
+    });
+  }
 
   entrar() {
     // aqui depois você pode validar login/token
